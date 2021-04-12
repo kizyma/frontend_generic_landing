d161740fcfee65b29b2f5ab8775e2832c627a4be
@yaroslav3 <yaroslav@skylum.com>
2021-04-12 19:33:43 +0300

    added score reset and finilized game logic

--- resources/js/pig_game.js
+++ resources/js/pig_game.js
@@ -11,13 +11,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-const score = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+let score, currentScore, activePlayer, playing;
+
+const init = function () {
+  // Starting conditions
+  score = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -29,40 +44,47 @@ const switchPlayer = function () {
 
 // Dice Roll functionality
 btnRoll.addEventListener('click', function () {
-  // generate rand number, I hate how rand works in JS, idiotic
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  // display according image
-  diceEl.classList.remove('hidden');
-  diceEl.src = `resources/img/game_resource_img/dice-${dice}.png`;
-  // chech if 1, if true switch player
-  if (dice !== 1) {
-    //  add score
-    currentScore += dice;
-    document.getElementById(
-      `current--${activePlayer}`
-    ).textContent = currentScore;
-  } else {
-    //   switch player
-    switchPlayer();
+  if (playing) {
+    // generate rand number, I hate how rand works in JS, idiotic
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // display according image
+    diceEl.classList.remove('hidden');
+    diceEl.src = `resources/img/game_resource_img/dice-${dice}.png`;
+    // chech if 1, if true switch player
+    if (dice !== 1) {
+      //  add score
+      currentScore += dice;
+      document.getElementById(
+        `current--${activePlayer}`
+      ).textContent = currentScore;
+    } else {
+      //   switch player
+      switchPlayer();
+    }
   }
 });
 
 btnHold.addEventListener('click', function () {
-  //  add current score to active player
-  score[activePlayer] += currentScore;
-  console.log(score[activePlayer]);
-  document.getElementById(`score--${activePlayer}`).textContent =
-    score[activePlayer];
-  // check win condition
-  if (score[activePlayer] >= 100) {
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add('player--winner');
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.remove('player--active');
-  } else {
-    // switch player
-    switchPlayer();
+  if (playing) {
+    //  add current score to active player
+    score[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
+    // check win condition
+    if (score[activePlayer] >= 100) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // switch player
+      switchPlayer();
+    }
   }
 });
+
+btnNew.addEventListener('click', init);
